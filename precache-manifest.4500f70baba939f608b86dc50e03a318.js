a01374c9b5681b235c2cef126a9ecfaa7f8329e7
tunajim <lesperancejames@gmail.com>
2022-01-02 19:45:14 -0800

    Updates

---
+++ precache-manifest.4500f70baba939f608b86dc50e03a318.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ccbd25bf34e142fa6e5",
+    "url": "/cv-project/static/css/main.316d1a7a.chunk.css"
+  },
+  {
+    "revision": "3ccbd25bf34e142fa6e5",
+    "url": "/cv-project/static/js/main.3ccbd25b.chunk.js"
+  },
+  {
+    "revision": "09d73b64ffe86023ac36",
+    "url": "/cv-project/static/js/1.09d73b64.chunk.js"
+  },
+  {
+    "revision": "191a181b6a79c31ed82c",
+    "url": "/cv-project/static/js/runtime~main.191a181b.js"
+  },
+  {
+    "revision": "48eae2a6d9d67d0d63654ccfe576a370",
+    "url": "/cv-project/static/media/addComponent.48eae2a6.png"
+  },
+  {
+    "revision": "41bf034ff88e30d5c90a8835989710f7",
+    "url": "/cv-project/static/media/email.41bf034f.png"
+  },
+  {
+    "revision": "8a5683e5d72c5e573b982556111b0f6d",
+    "url": "/cv-project/static/media/navigation.8a5683e5.png"
+  },
+  {
+    "revision": "d492efc706db983e74258dbd348f2208",
+    "url": "/cv-project/static/media/linkedin.d492efc7.png"
+  },
+  {
+    "revision": "624f06eec040b822532e94686bef4399",
+    "url": "/cv-project/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cv-project/precache-manifest.4500f70baba939f608b86dc50e03a318.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cv-project/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
